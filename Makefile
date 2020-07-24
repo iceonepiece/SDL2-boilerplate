b0ca87540626e98de4461ba7092f6723387480ea
iceonepiece <iceonepiece@gmail.com>
2020-07-25 06:53:18 +0700

    set up

---
+++ Makefile
@@ -0,0 +1,11 @@
+OBJS = ./src/*.cpp ./src/*/*.cpp
+CC = g++
+COMPILER_FLAGS = -w
+LINKER_FLAGS = -lSDL2 -lSDL2_image
+OBJ_NAME = game.exe
+
+build: $(OBJS)
+	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
+
+run:
+	./$(OBJ_NAME)
---
+++ src/core/Game.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+class Game {
+private:
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  int ticksLastFrame;
+
+public:
+  bool isRunning;
+
+  Game();
+  ~Game();
+
+  void initialize(int width, int height);
+  void processInput();
+  void update();
+  void render();
+  void destroy();
+};
